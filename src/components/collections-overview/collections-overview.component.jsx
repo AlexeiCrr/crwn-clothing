9e4b6c219c4d8639a5408d80588482fd7c37108e
AlexCrr <crudu-alexei@mail.ru>
2019-09-27 15:09:18 +0300

    move directory into shop data and into its own reducer, created respective selectors and updated corresponding components with new redux flow with directory/shop

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,23 @@
+// core
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+// utils
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
+// components
+import CollectionPreview from '../collection-preview/collection-preview.component';
+// styles
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionForPreview
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,18 @@
+// core
 import React from 'react';
-import shopData from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
+//components
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import Collection from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: shopData
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={Collection} />
+    </div>
+  );
+};
 
 export default ShopPage;
